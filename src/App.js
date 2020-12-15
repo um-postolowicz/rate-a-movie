15861f129b1b9b1df8eddb1adfcd026fddab49e9
Urszula <u.postolowicz@gmail.com>
2020-12-15 16:49:22 +0100

    Add Header component - HTML

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./Header";
 import Movie from "./Movie";
 
 const API_KEY = "8c8f7131";
-const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s="matrix"`;
+const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s="avengers"`;
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -23,17 +24,21 @@ function App() {
 
   return (
     <>
-      <h1 className="app-title">Find your Movie</h1>
+      {/* <h1 className="app-title">Rate your Movie</h1>
       <input className="search" type="text" />
+      <ul className="movies-list"> */}
+      <Header />
       <ul className="movies-list">
-        {movies.map((movie) => (
-          <Movie
-            key={movie.imdbID}
-            poster={movie.Poster}
-            title={movie.Title}
-            year={movie.Year}
-          />
-        ))}
+        {movies.map((movie) =>
+          movie.Type === "movie" ? (
+            <Movie
+              key={movie.imdbID}
+              poster={movie.Poster}
+              title={movie.Title}
+              year={movie.Year}
+            />
+          ) : null
+        )}
       </ul>
     </>
   );
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <h1 className="app-title">Rate your Movie</h1>
+      <p className="header-desc">
+        Have you just watched an amazing movie and want to share that delight
+        with other people? Or the movie was a disaster and you want to save
+        others from that nightmare? You are in the right place! Find a movie by
+        typing it's title below and feel free to rate it!
+      </p>
+      <input className="search" placeholder="Find a movie" type="text" />
+    </header>
+  );
+};
+
+export default Header;
