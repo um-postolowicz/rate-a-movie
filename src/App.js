d2cf9f870d4bfe6c9d1a55abd118f8fce0217355
Urszula <u.postolowicz@gmail.com>
2020-12-15 16:09:16 +0100

    Fetch the data from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+const API_KEY = "8c8f7131";
+const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s="matrix"`;
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Http error: ${response.status}`);
+        }
+      })
+      .then((response) => setMovies(response))
+      .catch((error) => console.log(`Błąd: ${error}`));
+  }, []);
+
   return (
     <div className="App">
       <h1>Hey</h1>
