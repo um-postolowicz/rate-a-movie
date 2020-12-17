03494ffbadaf5e5f72a5b16ea926616f60be7777
Urszula <u.postolowicz@gmail.com>
2020-12-17 12:55:48 +0100

    Add media queries for tablets

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,18 @@ const Header = () => {
         <div className="header__container">
           <h1 className="header__title">Rate your Movie</h1>
           <p className="header__desc">
-            Have you just watched an amazing movie and want to share that
-            delight with other people? Or the movie was a disaster and you want
-            to save others from that nightmare? You are in the right place! Find
-            a movie by typing it's title below and feel free to rate it!
+            <span>
+              Have you just watched an amazing movie and want to share that
+              delight with other people?
+            </span>{" "}
+            <span>
+              Or the movie was a disaster and you want to save others from that
+              nightmare?
+            </span>{" "}
+            <span>You are in the right place!</span>{" "}
+            <span>
+              Find a movie by typing it's title below and feel free to rate it!
+            </span>
           </p>
         </div>
       </div>
