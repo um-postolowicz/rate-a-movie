dcd0b29fb70f2808b2ffbe97546a438451567bf7
Urszula <u.postolowicz@gmail.com>
2020-12-21 14:00:59 +0100

    Add stars to rate movie and style it

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 
 const Movie = ({ poster, title, type, year }) => {
+  const [showRate, setShowRate] = useState(false);
+  const handleStarRate = (e) => {
+    e.preventDefault();
+    setShowRate(true);
+    console.log(e);
+  };
   return (
     <li className="movies__container">
       <h2 className="movies__title">{title}</h2>
@@ -8,7 +15,23 @@ const Movie = ({ poster, title, type, year }) => {
       <h3 className="movies__year">{year}</h3>
       <img alt="movie poster" className="movies__poster" src={poster} />
       <button className="movies__btn btn">See details</button>
-      <button className="movies__btn btn">Rate it!</button>
+      {showRate ? (
+        <div className="movies__stars">
+          <BsStar className="movies__star" />
+          <BsStar className="movies__star" />
+          <BsStar className="movies__star" />
+          <BsStar className="movies__star" />
+          <BsStar className="movies__star" />
+        </div>
+      ) : (
+        <button className="movies__btn btn" onClick={handleStarRate}>
+          Rate it!
+        </button>
+      )}
+
+      {/* <BsStar />
+      <BsStarFill />
+      <BsStarHalf /> */}
     </li>
   );
 };
