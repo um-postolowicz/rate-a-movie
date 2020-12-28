5371f726200bcc679075cc10227ee85edb3a7afd
Urszula <u.postolowicz@gmail.com>
2020-12-28 14:57:03 +0100

    Change one star to fill separately

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,10 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import Star from "./Star";
-import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 
 const Rate = () => {
   const ref = useRef(null);
-  const [isFill, setIsFill] = useState(false);
+  // const [isFill, setIsFill] = useState(false);
 
   // const handleRateStar = (e) => {
   //   const clickPosition = e.clientX;
@@ -18,26 +17,26 @@ const Rate = () => {
   //   }
   // };
 
-  const handleFill = (e) => {
-    // console.log(e.currentTarget.id);
-    const clickPosition = e.clientX;
-    const windowWidth = window.innerWidth;
-    const starsWidth = ref.current.offsetWidth;
-    const clickPositionInStarsDiv =
-      clickPosition - (windowWidth - starsWidth) / 2;
-    const starId = Math.ceil(clickPositionInStarsDiv / 25);
-    if (parseInt(e.currentTarget.id) === starId) {
-      setIsFill(!isFill);
-    }
-  };
+  // const handleFill = (e) => {
+  //   // console.log(e.currentTarget.id);
+  //   const clickPosition = e.clientX;
+  //   const windowWidth = window.innerWidth;
+  //   const starsWidth = ref.current.offsetWidth;
+  //   const clickPositionInStarsDiv =
+  //     clickPosition - (windowWidth - starsWidth) / 2;
+  //   const starId = Math.ceil(clickPositionInStarsDiv / 25);
+  //   if (parseInt(e.currentTarget.id) === starId) {
+  //     setIsFill(!isFill);
+  //   }
+  // };
 
   return (
     <div className="movies__stars" ref={ref}>
-      <Star id="1" isFill={isFill} onClick={handleFill} />
-      <Star id="2" isFill={isFill} onClick={handleFill} />
-      <Star id="3" isFill={isFill} onClick={handleFill} />
-      <Star id="4" isFill={isFill} onClick={handleFill} />
-      <Star id="5" isFill={isFill} onClick={handleFill} />
+      <Star id="1" />
+      <Star id="2" />
+      <Star id="3" />
+      <Star id="4" />
+      <Star id="5" />
     </div>
   );
 };
--- src/components/Star.js
+++ src/components/Star.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 
-const Star = ({ id, isFill, onClick }) => {
+const Star = ({ id }) => {
+  const [isFill, setIsFill] = useState(false);
+  const handleFill = (e) => {
+    // console.log(e.currentTarget.id);
+    const clickPosition = e.clientX;
+    const windowWidth = window.innerWidth;
+    // const starsWidth = ref.current.offsetWidth;
+    const starsWidth = 125;
+    const clickPositionInStarsDiv =
+      clickPosition - (windowWidth - starsWidth) / 2;
+    const starId = Math.ceil(clickPositionInStarsDiv / 25);
+    if (parseInt(e.currentTarget.id) === starId) {
+      setIsFill(!isFill);
+    }
+  };
+
   return (
-    <div className="movies__star" id={id} onClick={onClick}>
+    <div className="movies__star" id={id} onClick={handleFill}>
       {isFill ? <BsStarFill /> : <BsStar />}
     </div>
   );
