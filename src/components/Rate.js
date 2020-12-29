28a9f7e47c98ba5a5617e98d8f8cda6a87b1d807
Urszula <u.postolowicz@gmail.com>
2020-12-29 10:22:56 +0100

    Create rating system, working only once after clicking on it

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,8 +1,52 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Star from "./Star";
+import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
+
+const starsBasic = [
+  {
+    id: 1,
+    icon: <BsStar />,
+  },
+  {
+    id: 2,
+    icon: <BsStar />,
+  },
+  {
+    id: 3,
+    icon: <BsStar />,
+  },
+  {
+    id: 4,
+    icon: <BsStar />,
+  },
+  {
+    id: 5,
+    icon: <BsStar />,
+  },
+];
 
 const Rate = () => {
   const ref = useRef(null);
+  const [stars, setStars] = useState(starsBasic);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleRate = (e) => {
+    setIsClicked(true);
+    let table = stars;
+    const clickPosition = e.clientX;
+    const windowWidth = window.innerWidth;
+    const starsWidth = ref.current.offsetWidth;
+    const clickPositionInStarsDiv =
+      clickPosition - (windowWidth - starsWidth) / 2;
+    const starId = Math.ceil(clickPositionInStarsDiv / 25);
+    for (let i = 0; i < starId; i++) {
+      table[i].icon = <BsStarFill />;
+    }
+    table.map((element) => <li key={element.id}>{element.icon}</li>);
+    setStars(table);
+    console.log(stars);
+    return stars;
+  };
   // const [isFill, setIsFill] = useState(false);
 
   // const handleRateStar = (e) => {
@@ -31,13 +75,11 @@ const Rate = () => {
   // };
 
   return (
-    <div className="movies__stars" ref={ref}>
-      <Star id="1" />
-      <Star id="2" />
-      <Star id="3" />
-      <Star id="4" />
-      <Star id="5" />
-    </div>
+    <ul className="movies__stars" onClick={handleRate} ref={ref}>
+      {isClicked
+        ? stars.map((star) => <li key={star.id}>{star.icon}</li>)
+        : starsBasic.map((star) => <li key={star.id}>{star.icon}</li>)}
+    </ul>
   );
 };
 
